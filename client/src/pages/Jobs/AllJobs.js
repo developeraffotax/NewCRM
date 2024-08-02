e8f6a82fad6a56a2b7b57e54ce424a4eab167945
developeraffotax <developer@affotax.com>
2024-08-02 18:14:17 +0500

    friday1

--- client/src/pages/Jobs/AllJobs.js
+++ client/src/pages/Jobs/AllJobs.js
@@ -80,7 +80,6 @@ export default function AllJobs() {
     // eslint-disable-next-line
   }, []);
 
-<<<<<<< HEAD
   // ---------------All Client_Job Data----------->
   const allClientJobData = async () => {
     setLoading(true);
@@ -495,140 +494,6 @@ export default function AllJobs() {
     [users, play]
   );
 
-=======
-  // const columns = [
-  //   {
-  //     headerName: "Sr",
-  //     filter: false,
-  //     checkboxSelection: true,
-  //     headerCheckboxSelection: true,
-  //     editable: false,
-  //     valueGetter: (params) => params.node.rowIndex + 1,
-  //   },
-  //   {
-  //     headerName: "Company Name",
-  //     field: "companyName",
-  //     editable: false,
-  //     filter: true,
-  //   },
-  //   {
-  //     headerName: "Client Name",
-  //     field: "clientName",
-  //     filter: true,
-  //   },
-  //   {
-  //     headerName: "Job Holder",
-  //     field: "job.jobHolder",
-  //     filter: "agSetColumnFilter",
-  //     filterParams: {
-  //       values: jobHolders,
-  //       suppressMiniFilter: true,
-  //       suppressSelectAll: true,
-  //     },
-  //     floatingFilterComponent: "agSetColumnFloatingFilter",
-  //     floatingFilterComponentParams: {
-  //       values: jobHolders,
-  //       suppressFilterButton: true,
-  //       suppressInput: true,
-  //     },
-  //   },
-  //   {
-  //     headerName: "Departments",
-  //     field: "job.jobName",
-  //     filter: true,
-  //   },
-  //   {
-  //     headerName: "Hours",
-  //     field: "totalHours",
-  //     filter: true,
-  //   },
-  //   {
-  //     headerName: "Year End",
-  //     field: "job.yearEnd",
-  //     filter: true,
-  //   },
-  //   {
-  //     headerName: "Deadline",
-  //     field: "job.jobDeadline",
-  //     filter: true,
-  //   },
-  //   {
-  //     headerName: "Job Date",
-  //     field: "currentDate",
-  //     filter: true,
-  //   },
-  //   {
-  //     headerName: "Status",
-  //     field: "job.jobStatus",
-  //     filter: true,
-  //   },
-  //   {
-  //     headerName: "Note",
-  //     field: "job.note",
-  //     filter: true,
-  //   },
-  //   {
-  //     headerName: "Job Status",
-  //     field: "job.jobStatus",
-  //     cellEditor: "agSelectCellEditor",
-  //     cellEditorParams: {
-  //       values: [
-  //         "Select",
-  //         "Data",
-  //         "Progress",
-  //         "Queries",
-  //         "Approval",
-  //         "Submission",
-  //         "Billing",
-  //         "Feedback",
-  //       ],
-  //     },
-  //     floatingFilterComponent: "selectFloatingFilter",
-  //     floatingFilterComponentParams: {
-  //       options: [
-  //         "Data",
-  //         "Progress",
-  //         "Queries",
-  //         "Approval",
-  //         "Submission",
-  //         "Billing",
-  //         "Feedback",
-  //       ],
-  //       suppressFilterButton: true,
-  //       suppressInput: true,
-  //     },
-  //   },
-  //   {
-  //     headerName: "Lead",
-  //     field: "job.lead",
-  //     filter: true,
-  //   },
-  //   {
-  //     headerName: "Timer",
-  //     field: "job.timer",
-  //     filter: true,
-  //   },
-  //   {
-  //     headerName: "Chat",
-  //     field: "job.chat",
-  //     filter: true,
-  //   },
-  // ];
-
-  // const defaultColDef = useMemo(
-  //   () => ({
-  //     sortable: true,
-  //     filter: true,
-  //     floatingFilter: true,
-  //     editable: true,
-  //     resizable: true,
-  //   }),
-  //   []
-  // );
-
-  // const cellClickedListener = useCallback((event) => {}, []);
-
->>>>>>> 320833a98d1f83bc52d5f33af85d0e1ee44988d1
   return (
     <Layout>
       {loading ? (
@@ -746,34 +611,6 @@ export default function AllJobs() {
                 }}
               />
             </div>
-<<<<<<< HEAD
-=======
-          ))}
-          <span className="">
-            <LuPlusCircle className="h-5 w-5 text-gray-950 hover:text-orange-600 cursor-pointer" />
-          </span>
-        </div>
-        {/*  */}
-        <hr className="my-5 bg-gray-300 w-full h-[1px]" />
-        {/* ---------------------Table---------------- */}
-        <div className="w-full min-h-screen">
-          <div
-            className="ag-theme-alpine"
-            style={{ height: 600, width: "100%" }}
-          >
-            {/* <AgGridReact
-              rowData={clientJobs}
-              columnDefs={columns}
-              editType={"fullRow"}
-              rowSelection="multiple"
-              defaultColDef={defaultColDef}
-              animateRows={true}
-              pagination={true}
-              paginationPageSize={25}
-              onCellClicked={cellClickedListener}
-              suppressDragLeaveHidesColumns={true}
-            /> */}
->>>>>>> 320833a98d1f83bc52d5f33af85d0e1ee44988d1
           </div>
         </div>
       )}
